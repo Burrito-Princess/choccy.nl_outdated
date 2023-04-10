c573b41b24c2661f53bd0837f804929e97661cd7
Burrito-Princess <113419432+Burrito-Princess@users.noreply.github.com>
2023-04-10 21:59:45 +0200

    oops wrong path fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 app.use(express.static("public"));
-const port = 81;
+const port = 443;
 const server = http.createServer(app);
 
 server.listen(port, () => {
