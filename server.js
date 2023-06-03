588726f4aef6169e251da60a9190b530122fb817
Burrito-Princess <113419432+Burrito-Princess@users.noreply.github.com>
2023-06-03 21:37:57 +0200

    try_3_1

--- server.js
+++ server.js
@@ -22,22 +22,37 @@ const server = http.createServer(function(request, response) {
 });
 
 function serveStaticFile(filePath, response) {
-  const file = path.join(__dirname, filePath);
-
-  fs.readFile(file, function(error, content) {
-    if (error) {
-      response.statusCode = 500;
-      response.setHeader('Content-Type', 'text/html');
-      response.write('<p>Error reading file.</p>');
-      response.end();
-    } else {
-      response.statusCode = 200;
-      response.setHeader('Content-Type', 'text/html');
-      response.write(content);
-      response.end();
+    const fileExtension = path.extname(filePath);
+    const contentType = getContentType(fileExtension);
+    const file = path.join(__dirname, filePath);
+  
+    fs.readFile(file, function(error, content) {
+      if (error) {
+        response.statusCode = 500;
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<p>Error reading file.</p>');
+        response.end();
+      } else {
+        response.statusCode = 200;
+        response.setHeader('Content-Type', contentType);
+        response.write(content);
+        response.end();
+      }
+    });
+  }
+  
+  function getContentType(fileExtension) {
+    switch (fileExtension) {
+      case '.html':
+        return 'text/html';
+      case '.css':
+        return 'text/css';
+      case '.js':
+        return 'text/javascript';
+      default:
+        return 'text/plain';
     }
-  });
-}
+  }
 
 server.listen(port, () => {
     console.log("listening on " + port);
