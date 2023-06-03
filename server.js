9ab363ebc288c13c9bb62267bfd8c3c2ad24b07c
Burrito-Princess <113419432+Burrito-Princess@users.noreply.github.com>
2023-06-03 16:52:37 +0200

    TRY_VIR_2

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.use(express.static("public"));
 const port = 80;
 const server = http.createServer(function(request, response) {
     switch(request.headers.host) {
-        case 'choccy.nl': response.write('<script>app.use(express.static("public"));</script>'); break;
-        case 'official-lou.nl': response.write('<h1>lou/h1>'); break;
+        case 'choccy.nl': response.write("<meta http-equiv=\"refresh \" content=\"0; 'public'\" />"); break;
+        case 'official-lou.nl': response.write('<h1>lou</h1>'); break;
         default: 
             response.statusCode = 404;
             response.write('<p>We do not serve the host: <b>' + request.headers.host + '</b>.</p>');
