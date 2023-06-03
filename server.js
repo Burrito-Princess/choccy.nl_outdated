c8b095569969b458d21c99125db3b4129bb4db16
Burrito-Princess <113419432+Burrito-Princess@users.noreply.github.com>
2023-06-03 21:15:20 +0200

    TRY_VIR_3

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-app.use(express.static("public"));
 const port = 80;
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 const server = http.createServer(function(request, response) {
-    switch(request.headers.host) {
-        case 'choccy.nl': response.write("<meta http-equiv=\"refresh \" content=\"0; 'public'\" />"); break;
-        case 'official-lou.nl': response.write('<h1>lou</h1>'); break;
-        default: 
-            response.statusCode = 404;
-            response.write('<p>We do not serve the host: <b>' + request.headers.host + '</b>.</p>');
+  const hostname = request.headers.host.split(':')[0];
+  
+  switch (hostname) {
+    case 'choccy.nl':
+      serveStaticFile('public/index.html', response);
+      break;
+    case 'official-lou.nl':
+      serveStaticFile('official-lou/index.html', response);
+      break;
+    default:
+      response.statusCode = 404;
+      response.setHeader('Content-Type', 'text/html');
+      response.write(`<p>We do not serve the host: <b>${hostname}</b>.</p>`);
+      response.end();
+  }
+});
+
+function serveStaticFile(filePath, response) {
+  const file = path.join(__dirname, filePath);
+
+  fs.readFile(file, function(error, content) {
+    if (error) {
+      response.statusCode = 500;
+      response.setHeader('Content-Type', 'text/html');
+      response.write('<p>Error reading file.</p>');
+      response.end();
+    } else {
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/html');
+      response.write(content);
+      response.end();
     }
-    response.end();
+  });
 }
-
-
-);
 
 server.listen(port, () => {
     console.log("listening on " + port);
