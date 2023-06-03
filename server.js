a1f601c9a1c45c69c49c9bffada8e0c7dfedc482
Burrito-Princess <113419432+Burrito-Princess@users.noreply.github.com>
2023-06-03 15:42:12 +0200

    AFTER_virtual

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ const app = express();
 const http = require('http');
 app.use(express.static("public"));
 const port = 80;
-const server = http.createServer(app);
+const server = http.createServer(function(request, response) {
+    switch(request.headers.host) {
+        case 'choccy.nl': response.write('<script>app.use(express.static("public"));</script>'); break;
+        case 'not.choccy.nl': response.write('<h1>oops/h1>'); break;
+        default: 
+            response.statusCode = 404;
+            response.write('<p>We do not serve the host: <b>' + request.headers.host + '</b>.</p>');
+    }
+    response.end();
+}
+
+
+);
 
 server.listen(port, () => {
     console.log("listening on " + port);
